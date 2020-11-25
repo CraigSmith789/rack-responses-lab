1d26fdf3608aaaf14f143aa5c33c34abaeae4caf
CraigSmith789 <craig789@gmail.com>
2020-11-25 18:05:50 -0500

    commit tree adc7ce08b6bb4437539d711bfe0bb44cb6ccb38d

---
+++ app/application.rb
@@ -0,0 +1,17 @@
+
+class Application 
+  def call(env)
+    resp = Rack::Response.new
+    t = Time.now
+  # binding.pry
+    if t.hour <= 12
+  # binding.pry
+    resp.write "Good Morning!"
+    else
+    resp.write "Good Afternoon!"
+    
+    end
+    resp.finish
+  end
+end
+  
